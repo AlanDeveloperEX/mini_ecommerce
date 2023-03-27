877b40b5f0986c36edd19a59df9b25127ac60269
Alan Santos <alan.santos.ex.contato@gmail.com>
2023-03-27 14:45:56 -0300

    feat:(root base theme) add theme rule and pages folder

---
+++ frontend/src/global/styles/theme.ts
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    default_white: "#fff",
+    default_text: "#2F2E41",
+    default_background: "#2F2E41",
+    background_btn: "#2F2E41",
+    background_btn_act: "#039B00",
+  },
+  shape: {
+    cards: {
+      default: "border-radius: 4px; padding: 10px 12px;",
+    },
+    buttons: {
+      default: "padding: 8px; border-radius: 4px; height: 40px;",
+    },
+  },
+};
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+const Home = () => {
+  return (
+    <S.MainContainer>
+      <p>test</p>
+    </S.MainContainer>
+  );
+};
+
+export default Home;
